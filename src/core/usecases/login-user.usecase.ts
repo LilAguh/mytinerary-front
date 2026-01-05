fa40f052e79fefb5cfb3baa0f730f001e19543f4
LilAguh <aguezeochoa@gmail.com>
2026-01-05 19:58:25 -0300

    feat(core): descripcion de responsabilidades del usecase loginuser

---
+++ src/core/usecases/login-user.usecase.ts
@@ -0,0 +1,6 @@
+// LoginUser
+// 1. valida email y password
+// 2. consulta AuthRepository
+// 3. decide si el login es válido
+// 4. guarda sesión si corresponde
+// 5. devuelve usuario autenticado o lanza error
